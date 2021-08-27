7caa01d87a65443c918866b41cea5ae3e1579e99
Sejong Jeong <wjdtpwhd@gmail.com>
2021-08-27 17:11:54 +0900

    First Setting

---
+++ data_loader/data_loaders.py
@@ -0,0 +1,34 @@
+from torchvision import datasets, transforms
+from base import BaseDataLoader
+
+### Configurations
+data_dir = '/opt/ml/input/data/train'
+img_dir = f'{data_dir}/images'
+df_path = f'{data_dir}/train.csv'
+
+df = pd.read_csv(df_path)
+
+class MaskLabels:
+    mask = 0
+    incorrect = 1
+    normal = 2
+
+class GenderLabels:
+    male = 0
+    female = 1
+
+class AgeGroup:
+    map_label = lambda x: 0 if int(x) < 30 else 1 if int(x) < 60 else 2
+
+class MnistDataLoader(BaseDataLoader):
+    """
+    MNIST data loading demo using BaseDataLoader
+    """
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+        trsfm = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])
+        self.data_dir = data_dir
+        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
