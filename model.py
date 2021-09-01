ea0e7162deaa814e86e9340679036b6f85161117
jiwoo <ann990212@gmail.com>
2021-09-01 22:24:26 +0900

    import timm

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 from torchvision import models
+import timm
+
 
 class VGG(nn.Module):
     def __init__(self, num_class):
@@ -12,17 +14,18 @@ class VGG(nn.Module):
             nn.ReLU(True),
             nn.Dropout(),
             nn.Linear(in_features=64, out_features=num_class)
-        ) 
-        
-    def forward(self,x):
+        )
+
+    def forward(self, x):
         out = self.vgg16(x)
         out = self.classifier(out)
         return out
-    
+
+
 class RESNET152(torch.nn.Module):
-    def __init__(self,num_class):
-        super(RESNET152,self).__init__()
-        
+    def __init__(self, num_class):
+        super(RESNET152, self).__init__()
+
         self.resnet152 = models.resnet152(pretrained=True)
         self.classifier = nn.Sequential(
             nn.Linear(in_features=1000, out_features=64),
@@ -30,17 +33,20 @@ class RESNET152(torch.nn.Module):
             nn.Dropout(),
             nn.Linear(in_features=64, out_features=num_class)
         )
+
     def forward(self, x):
         out = self.resnet152(x)
         out = self.classifier(out)
         return out
-    
+
+
 class ViT(nn.Module):
     def __init__(self, num_class):
         super(ViT, self).__init__()
-        
-        self.vit = timm.create_model('vit_base_patch32_224', pretrained=True, num_classes=num_class)
-    
+
+        self.vit = timm.create_model(
+            'vit_base_patch32_224', pretrained=True, num_classes=num_class)
+
     def forward(self, x):
         out = self.vit(x)
         return out
